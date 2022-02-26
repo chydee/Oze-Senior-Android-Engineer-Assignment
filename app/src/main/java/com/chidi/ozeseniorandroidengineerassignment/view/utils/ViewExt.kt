de5f483ecb1407ecbc108f65d0584f554f0845c0
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2022-02-26 14:39:18 +0100

    create and add HomeFragment create and add View extensions in ViewExt.kt add AutoClearedValue.kt

---
+++ app/src/main/java/com/chidi/ozeseniorandroidengineerassignment/view/utils/ViewExt.kt
@@ -0,0 +1,16 @@
+package com.chidi.ozeseniorandroidengineerassignment.view.utils
+
+import android.view.View
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+
+    this.visibility = View.GONE
+}
